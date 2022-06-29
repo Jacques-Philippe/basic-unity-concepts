effeff84069e6bed0108a7f25405b98cc366c818
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-06-28 20:03:14 -0400

    Implement basic 2d movement

---
+++ Movement.Unity/Assets/2d/Scripts/PlayerMovementManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _2d
+{
+    public class PlayerMovementManager : MonoBehaviour
+    {
+        [Tooltip("The Player transform to move")]
+        [SerializeField]
+        private Transform Player;
+        [Tooltip("The speed by which to move the player")]
+        public float Speed = 5;
+
+        private float horizontalMovement;
+
+        public bool IsMovingRight { get => this.horizontalMovement > 0; }
+        public bool IsMovingLeft { get => this.horizontalMovement < 0; }
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            //a float value contained in [-1, 1]
+            var horizontalInput = Input.GetAxis("Horizontal");
+            this.horizontalMovement = this.Speed * horizontalInput * Time.deltaTime;
+            this.Player.position += this.Player.right * horizontalMovement;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Purpose
 A repository of different movement schemes
+
+# Get started (dev)
+1. Install .NET SDK
+1. Execute `dev/get-started`
