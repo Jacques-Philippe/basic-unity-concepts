2ba1230d25567ab3107e68fb82d6ce52b7d3de57
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-07-01 13:58:55 -0400

    Configure animator state and fix jump bug
    - Ensure consistent animator state with respect to where we're at in the jump
    - Fix issue wherein spamming space allows the player's initial height to
      increase

--- Movement.Unity/Assets/2d/Scripts/PlayerMovementManager.cs
+++ Movement.Unity/Assets/2d/Scripts/PlayerMovementManager.cs
@@ -22,38 +22,55 @@ namespace _2d
         [SerializeField]
         private Animator playerAnimator;
 
+        private bool isMidJump = false;
+
         IEnumerator Jump()
         {
+            isMidJump = true;
             var verticalVelocity = JumpSpeed;
             var gravity = -9.8f;
             var initialHeight = this.Player.transform.position.y;
-            // this.playerAnimator.SetBool("IsJumping", true);
+
             //Up
-            yield return new WaitUntil(delegate ()
-            {
-                this.Player.position += this.Player.transform.up * verticalVelocity * Time.deltaTime;
-                verticalVelocity += gravity * Time.deltaTime;
-                //Clamp vertical velocity to 0 if it becomes negative
-                if (verticalVelocity < 0) verticalVelocity = 0;
-                return verticalVelocity == 0;
-            });
-
-            // this.playerAnimator.SetBool("IsFalling", true);
-            //Down
-            yield return new WaitUntil(delegate ()
-            {
-                this.Player.position += this.Player.transform.up * verticalVelocity * Time.deltaTime;
-                verticalVelocity += gravity * Time.deltaTime;
-                var currentHeight = this.Player.position.y;
-                //Clamp player fall to the initial height
-                if (currentHeight < initialHeight)
+            yield return new WaitUntil(
+                delegate ()
                 {
-                    this.Player.position = new Vector3(this.Player.position.x, initialHeight, this.Player.position.z);
+                    this.Player.position +=
+                        this.Player.transform.up * verticalVelocity * Time.deltaTime;
+                    verticalVelocity += gravity * Time.deltaTime;
+                    //Clamp vertical velocity to 0 if it becomes negative
+                    if (verticalVelocity < 0)
+                        verticalVelocity = 0;
+
+                    this.playerAnimator.SetBool("IsJumping", verticalVelocity > 0);
+                    return verticalVelocity == 0;
                 }
-                return Player.position.y == initialHeight;
-            });
+            );
 
 
+            //Down
+            yield return new WaitUntil(
+                delegate ()
+                {
+                    this.Player.position +=
+                        this.Player.transform.up * verticalVelocity * Time.deltaTime;
+                    verticalVelocity += gravity * Time.deltaTime;
+                    var currentHeight = this.Player.position.y;
+                    //Clamp player fall to the initial height
+                    if (currentHeight < initialHeight)
+                    {
+                        this.Player.position = new Vector3(
+                            this.Player.position.x,
+                            initialHeight,
+                            this.Player.position.z
+                        );
+                    }
+
+                    this.playerAnimator.SetBool("IsFalling", Player.position.y > initialHeight);
+                    return Player.position.y == initialHeight;
+                }
+            );
+            isMidJump = false;
             yield return null;
         }
 
@@ -63,10 +80,11 @@ namespace _2d
         // Update is called once per frame
         void Update()
         {
-
-            if (Input.GetKeyDown(KeyCode.Space)) StartCoroutine(Jump());
+            if (!this.isMidJump && Input.GetKeyDown(KeyCode.Space))
+                StartCoroutine(Jump());
 
             //a float value contained in [-1, 1]
+            //has value 0 for no input
             var horizontalInput = Input.GetAxis("Horizontal");
 
             var horizontalMovement = this.Speed * horizontalInput * Time.deltaTime;
